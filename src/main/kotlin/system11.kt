dd29ab6ed92569549ab14506aab548339fdc1b62
gpfla6022 <gpfla3503@gmail.com>
2021-04-22 21:14:01 +0900

    게시물 리스팅, 최신순으로 정렬

---
+++ src/main/kotlin/system11.kt
@@ -0,0 +1,147 @@
+import java.text.SimpleDateFormat
+
+fun main() {
+    println("== 게시판 관리 프로그램 시작 ==")
+
+    makeTestArticles();
+
+    loop@ while (true) {
+        print("명령어) ")
+        val command = readLineTrim()
+
+        when {
+            command == "system exit" -> {
+                println("프로그램을 종료합니다.")
+                break@loop
+            }
+            command.startsWith("article delete ") -> {
+                val id = command.trim().split(" ")[2].toInt()
+
+                var articleToDelete = getArticleById(id)
+
+                if (articleToDelete == null) {
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue
+                }
+
+                articles.remove(articleToDelete)
+
+                println("${id}번 게시물을 삭제하였습니다.")
+            }
+            command.startsWith("article modify ") -> {
+                val id = command.trim().split(" ")[2].toInt()
+
+                var articleToModify = getArticleById(id)
+
+                if (articleToModify == null) {
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue
+                }
+
+                print("${id}번 게시물 새 제목 : ")
+                articleToModify.title = readLineTrim()
+                print("${id}번 게시물 새 내용 : ")
+                articleToModify.body = readLineTrim()
+                articleToModify.updateDate = Util.getNowDateStr()
+
+                println("${id}번 게시물을 수정하였습니다.")
+            }
+            command.startsWith("article detail ") -> {
+                val id = command.trim().split(" ")[2].toInt()
+
+                var article = getArticleById(id)
+
+                if (article == null) {
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue
+                }
+
+                println("번호 : ${article.id}")
+                println("작성날짜 : ${article.regDate}")
+                println("갱신날짜 : ${article.updateDate}")
+                println("제목 : ${article.title}")
+                println("내용 : ${article.body}")
+            }
+            command == "article write" -> {
+                print("제목 : ")
+                val title = readLineTrim()
+                print("내용 : ")
+                val body = readLineTrim()
+
+                val id = addArticle(title, body)
+
+                println("${id}번 게시물이 작성되었습니다.")
+            }
+            command == "article list" -> {
+                println("번호 / 작성날짜 / 제목")
+
+                for (article in articles.reversed()) {
+                    println("${article.id} / ${article.regDate} / ${article.title}")
+                }
+            }
+            else -> {
+                println("`$command` 은(는) 존재하지 않는 명령어 입니다.")
+            }
+        }
+    }
+
+    println("== 게시판 관리 프로그램 끝 ==")
+}
+/* 게시물 관련 시작 */
+// 가장 마지막에 입력된 게시물 번호
+var articlesLastId = 0
+
+val articles = mutableListOf<Article>()
+
+fun getArticleById(id: Int): Article? {
+    for (article in articles) {
+        if (article.id == id) {
+            return article
+        }
+    }
+
+    return null
+}
+
+fun addArticle(title: String, body: String): Int {
+    val id = articlesLastId + 1
+    val regDate = Util.getNowDateStr()
+    val updateDate = Util.getNowDateStr()
+
+    val article = Article(id, regDate, updateDate, title, body)
+    articles.add(article)
+
+    articlesLastId = id
+
+    return id
+}
+
+fun makeTestArticles() {
+    for ( id in 1 .. 100 ) {
+        val title = "제목_$id"
+        val body = "내용_$id"
+
+        addArticle(title, body)
+    }
+}
+
+data class Article(
+    val id: Int,
+    val regDate: String,
+    var updateDate: String,
+    var title: String,
+    var body: String
+)
+/* 게시물 관련 끝 */
+
+/* 유틸관련 시작 */
+fun readLineTrim() = readLine()!!.trim()
+
+object Util {
+    fun getNowDateStr(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+        return dateFormat.format(System.currentTimeMillis())
+    }
+}
+/* 유틸관련 끝 */
