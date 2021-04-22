e67100e61bdc8d079c31fc6d9174c86f3578d93b
gpfla6022 <gpfla3503@gmail.com>
2021-04-22 20:31:47 +0900

    게시물 수정기능

---
+++ src/main/kotlin/system7.kt
@@ -0,0 +1,111 @@
+import java.text.SimpleDateFormat
+
+fun readLineTrim() = readLine()!!.trim()
+
+var articlesLastId = 0
+
+val articles = mutableListOf<Article>()
+
+fun getArticleById(id: Int): Article? {
+    for ( article in articles ) {
+        if (article.id == id ) {
+            return article
+        }
+    }
+    return null
+}
+
+fun main() {
+    println("== 게시판 관리 프로그램 시작 ==")
+
+    loop@ while (true) {
+        print("명령어")
+        val command = readLineTrim()
+        println("입력한 명령어 : $command")
+
+        when {
+            command == "system exit" -> {
+                println("프로그램을 종료합니다.")
+                break@loop
+            }
+
+            command.startsWith("article delete ") -> {
+                val id = command.trim().split(" ")[2].toInt()
+
+                var articleToDelete = getArticleById(id)
+
+                if( articleToDelete == null){
+                    print("${id}번 게시물은 존재하지 않습니다.")
+                    continue
+                }
+
+                articles.remove(articleToDelete)
+
+                print("${id}번 게시물을 삭제합니다.")
+            }
+            command.startsWith("article modify ") -> {
+                val id = command.trim().split(" ")[2].toInt()
+
+                var articleToModify = getArticleById(id)
+
+                if( articleToModify == null){
+                    println("${id}번 게시물은 존재하지 않습니다.")
+                    continue
+                }
+
+                print("${id}번 게시물 새 제목")
+                articleToModify.title = readLineTrim()
+                print("${id}번 게시물 새 내용")
+                articleToModify.body = readLineTrim()
+                articleToModify.updateDate = Util.getNowDateStr()
+
+                print("${id}번 게시물을 수정하였습니다.")
+            }
+
+
+            command == "article write" -> {
+                val id = articlesLastId + 1
+                val regDate = Util.getNowDateStr()
+                val updateDate = Util.getNowDateStr()
+
+
+                print("제목 : ")
+                val title = readLineTrim()
+                print("내용 : ")
+                val body = readLineTrim()
+                val article = Article(id, regDate, updateDate, title, body)
+
+                println("$id 번 게시물이 작성되었습니다.")
+
+                articles.add(article)
+
+                articlesLastId = id
+            }
+            command == "article List" -> {
+                println("번호 / 작성날짜 / 제목 ")
+
+                for( article in articles ) {
+                    println("${article.id} / ${article.regDate} / ${article.title}")
+                }
+            }
+            else -> {
+                println("'$command' 은(는) 존재하지 않는 명령어 입니다.")
+            }
+        }
+    }
+
+    println("== 게시판 관리 프로그램 끝 ==")
+
+}
+data class Article(
+    val id: Int,
+    val regDate: String, //등록일(바뀌지 않음)
+    var updateDate: String, //수정일(바뀜)
+    var title: String,
+    var body: String
+)
+
+object Util {
+    fun getNowDateStr() = SimpleDateFormat("yyyy-mm-dd HH:mm:ss").format(System.currentTimeMillis())
+
+}
